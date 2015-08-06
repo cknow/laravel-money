2ed42368c20f3097a3b28c3d8c86e06562ec332a
Ricardo Gobbo <ricardo@clicknow.com.br>
2015-08-06 13:10:52 -0300

    Add HelpersTest

---
+++ tests/HelpersTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use \Clicknow\Money\Currency;
+use \Clicknow\Money\Money;
+
+class HelpersTest extends PHPUnit_Framework_TestCase
+{
+    public function testMoney()
+    {
+        $this->assertEquals(new Currency('BRL'), currency('BRL'));
+        $this->assertEquals(new Currency('USD'), currency('USD'));
+    }
+
+    public function testCurrency()
+    {
+        $this->assertEquals(new Money(25, new Currency('BRL')), money(25, 'BRL'));
+        $this->assertEquals(new Money(25, new Currency('USD')), money(25, 'USD'));
+    }
+}
