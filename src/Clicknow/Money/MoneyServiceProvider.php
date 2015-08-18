26d312a5d85d3ec3654e6cafa3cfa4c52a1e0e9d
Ricardo Gobbo <ricardo@clicknow.com.br>
2015-08-18 17:01:12 -0300

    Add group in publish

--- src/Clicknow/Money/MoneyServiceProvider.php
+++ src/Clicknow/Money/MoneyServiceProvider.php
@@ -19,7 +19,7 @@ class MoneyServiceProvider extends ServiceProvider
 
         $this->publishes([
             $config => $this->app->make('path.config').'/clicknow.money.php',
-        ]);
+        ], "config");
 
         Money::setLocale($this->app->make('translator')->getLocale());
         Currency::setCurrencies($this->app->make('config')->get('clicknow.money'));
