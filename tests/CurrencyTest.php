b02cadb5bdec6d67a9322b452638276cc0b5da0c
Ricardo Gobbo <ricardo@clicknow.com.br>
2016-08-03 13:42:47 -0300

    Add more tests

--- tests/CurrencyTest.php
+++ tests/CurrencyTest.php
@@ -31,22 +31,38 @@ class CurrencyTest extends PHPUnit_Framework_TestCase
 
     public function testGetters()
     {
-        $c = new Currency('BRL');
+        $c1 = new Currency('BRL');
 
-        $this->assertEquals('BRL', $c->getCurrency());
-        $this->assertEquals('Brazilian Real', $c->getName());
-        $this->assertEquals(986, $c->getCode());
-        $this->assertEquals(2, $c->getPrecision());
-        $this->assertEquals(100, $c->getSubunit());
-        $this->assertEquals('R$', $c->getSymbol());
-        $this->assertEquals(true, $c->isSymbolFirst());
-        $this->assertEquals(',', $c->getDecimalMark());
-        $this->assertEquals('.', $c->getThousandsSeparator());
-        $this->assertEquals('R$ ', $c->getPrefix());
-        $this->assertEquals('', $c->getSuffix());
-        $this->assertNotEmpty($c->toArray()['BRL']);
-        $this->assertJson($c->toJson());
-        $this->assertNotEmpty($c->jsonSerialize()['BRL']);
+        $this->assertEquals('BRL', $c1->getCurrency());
+        $this->assertEquals('Brazilian Real', $c1->getName());
+        $this->assertEquals(986, $c1->getCode());
+        $this->assertEquals(2, $c1->getPrecision());
+        $this->assertEquals(100, $c1->getSubunit());
+        $this->assertEquals('R$', $c1->getSymbol());
+        $this->assertEquals(true, $c1->isSymbolFirst());
+        $this->assertEquals(',', $c1->getDecimalMark());
+        $this->assertEquals('.', $c1->getThousandsSeparator());
+        $this->assertEquals('R$ ', $c1->getPrefix());
+        $this->assertEquals('', $c1->getSuffix());
+        $this->assertNotEmpty($c1->toArray()['BRL']);
+        $this->assertJson($c1->toJson());
+        $this->assertNotEmpty($c1->jsonSerialize()['BRL']);
+
+        $c2 = new Currency('CDF');
+        $this->assertEquals('CDF', $c2->getCurrency());
+        $this->assertEquals('Congolese Franc', $c2->getName());
+        $this->assertEquals(976, $c2->getCode());
+        $this->assertEquals(2, $c2->getPrecision());
+        $this->assertEquals(100, $c2->getSubunit());
+        $this->assertEquals('Fr', $c2->getSymbol());
+        $this->assertEquals(false, $c2->isSymbolFirst());
+        $this->assertEquals('.', $c2->getDecimalMark());
+        $this->assertEquals(',', $c2->getThousandsSeparator());
+        $this->assertEquals('', $c2->getPrefix());
+        $this->assertEquals('Fr', $c2->getSuffix());
+        $this->assertNotEmpty($c2->toArray()['CDF']);
+        $this->assertJson($c2->toJson());
+        $this->assertNotEmpty($c2->jsonSerialize()['CDF']);
     }
 
     public function testToString()
