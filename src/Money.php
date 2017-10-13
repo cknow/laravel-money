d24ae4fea9898b0ab6db47db58326a8ea3095991
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2017-10-13 18:04:27 -0300

    Added parser

--- src/Money.php
+++ src/Money.php
@@ -10,7 +10,10 @@ use Money\Currencies;
 use Money\Currency;
 use Money\Formatter\DecimalMoneyFormatter;
 use Money\Formatter\IntlMoneyFormatter;
+use Money\Parser\DecimalMoneyParser;
+use Money\Parser\IntlMoneyParser;
 use Money\MoneyFormatter;
+use Money\MoneyParser;
 use NumberFormatter;
 
 /**
@@ -228,6 +231,54 @@ class Money implements Arrayable, Jsonable, JsonSerializable, Renderable
     public static function convert(\Money\Money $intance)
     {
         return new self($intance->getAmount(), $intance->getCurrency());
+    }
+
+    /**
+     * Parse.
+     *
+     * @param string            $money
+     * @param string|null       $forceCurrency
+     * @param string|null       $locale
+     * @param \Money\Currencies $currencies
+     *
+     * @return \Cknow\Money\Money
+     */
+    public static function parse($money, $forceCurrency = null, $locale = null, Currencies $currencies = null)
+    {
+        $numberFormatter = new NumberFormatter($locale ?: static::getLocale(), NumberFormatter::CURRENCY);
+        $parser = new IntlMoneyParser($numberFormatter, $currencies ?: static::getCurrencies());
+
+        return Money::parseByParser($parser, $money, $forceCurrency);
+    }
+
+    /**
+     * Parse by decimal.
+     *
+     * @param string            $money
+     * @param string|null       $forceCurrency
+     * @param \Money\Currencies $currencies
+     *
+     * @return \Cknow\Money\Money
+     */
+    public static function parseByDecimal($money, $forceCurrency = null, Currencies $currencies = null)
+    {
+        $parser = new DecimalMoneyParser($currencies ?: static::getCurrencies());
+
+        return Money::parseByParser($parser, $money, $forceCurrency);
+    }
+
+    /**
+     * Parse by parser.
+     *
+     * @param \Money\MoneyParser $parser
+     * @param string             $money
+     * @param string|null        $forceCurrency
+     *
+     * @return \Cknow\Money\Money
+     */
+    public static function parseByParser(MoneyParser $parser, $money, $forceCurrency = null)
+    {
+        return Money::convert($parser->parse($money, $forceCurrency));
     }
 
     /**
