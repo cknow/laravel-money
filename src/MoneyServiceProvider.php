188a458573e8969631a95eb332e7e8f02de5dfb7
jamiehd <mail@jamiehd.co.uk>
2018-09-18 08:29:02 +0100

    Add a sumMoney method to collections
    This allows $collection->sumMoney('attribute') to be used to get a total of money attributes.

--- src/MoneyServiceProvider.php
+++ src/MoneyServiceProvider.php
@@ -13,5 +13,15 @@ class MoneyServiceProvider extends ServiceProvider
     {
         BladeExtension::register($this->app->make('blade.compiler'));
         Money::setLocale($this->app->make('translator')->getLocale());
+        
+        Collection::macro('sumMoney', function ($callback) {
+            $callback = $this->valueRetriever($callback);
+            return $this->reduce(function (Money $result, $item) use ($callback) {
+            if (!$amount = $callback($item)) {
+                $amount = money('0');
+            }
+            return $result->add($amount);
+            }, money('0'));
+        });
     }
 }
