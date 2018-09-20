0e8e4dd8cb8c98aa5cd1fb7f9c45dfba38f8465d
Ricardo Gobbo de Souza <ricardo@clicknow.com.br>
2018-09-20 09:39:30 -0300

    Add money factory

---
+++ tests/MoneyFactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cknow\Money;
+
+use Money\Currencies\ISOCurrencies;
+use Money\Currency;
+
+/**
+ * @covers \Cknow\Money\MoneyFactory
+ */
+class MoneyFactoryTest extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp()
+    {
+        Money::setCurrencies(new ISOCurrencies());
+        Money::setLocale('en_US');
+    }
+
+    public function testFactoryMethods()
+    {
+        $money = Money::USD(10);
+
+        static::assertInstanceOf(Money::class, $money);
+        static::assertEquals(new Money(10, new Currency('USD')), $money);
+    }
+}
