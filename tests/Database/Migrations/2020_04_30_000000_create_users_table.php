b8fc168953498be7ad04a42587aef49d490e22da
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2020-05-02 12:23:08 +1000

    Create testing assets

---
+++ tests/Database/Migrations/2020_04_30_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('money', 15, 2);
+            $table->decimal('wage', 15, 2);
+            $table->decimal('debits', 15, 2)->nullable();
+            $table->string('currency');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ tests/Database/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Cknow\Money\Database\Models;
+
+use Cknow\Money\MoneyCast;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * The testing user model.
+ *
+ */
+class User extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'money',
+        'wage',
+        'debits',
+        'currency',
+    ];
+
+    /**
+     * The attributes to cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'money' => MoneyCast::class,
+        'wage' => MoneyCast::class . ':EUR',
+        'debits' => MoneyCast::class . ':currency',
+    ];
+}
