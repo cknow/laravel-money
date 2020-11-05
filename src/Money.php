8c471deda3de23b0403dea56a60ef30f8cb3adcf
Babichev Maxim <maksim.babichev95@gmail.com>
2020-11-05 20:00:16 +0300

    Update Money.php

--- src/Money.php
+++ src/Money.php
@@ -23,7 +23,6 @@ class Money implements Arrayable, Jsonable, JsonSerializable, Renderable
     use MoneyParserTrait;
     use Macroable {
         Macroable::__call as macroCall;
-        Macroable::__callStatic as macroableCallStatic;
     }
 
     /**
