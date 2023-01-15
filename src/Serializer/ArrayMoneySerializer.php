5eb700740084d2a2816742c160e32d03d1c0f360
Austin Drummond <adrum0x7@gmail.com>
2023-01-15 13:10:42 -0500

    fix return types

--- src/Serializer/ArrayMoneySerializer.php
+++ src/Serializer/ArrayMoneySerializer.php
@@ -11,7 +11,7 @@ class ArrayMoneySerializer implements MoneySerializer
      * Formats a Money object as string.
      *
      * @param  \Money\Money  $moeny
-     * @return string
+     * @return mixed
      */
     public function serialize(Money $money): mixed
     {
