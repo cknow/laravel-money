936c409c39389e846f9d3f13c7e9881f132ea1a6
Austin Drummond <adrum0x7@gmail.com>
2023-04-08 16:23:12 -0400

    fix typos

--- src/Serializer/ArrayMoneySerializer.php
+++ src/Serializer/ArrayMoneySerializer.php
@@ -10,7 +10,7 @@ class ArrayMoneySerializer implements MoneySerializer
     /**
      * Formats a Money object as string.
      *
-     * @param  \Money\Money  $moeny
+     * @param  \Money\Money  $money
      * @return mixed
      */
     public function serialize(Money $money): mixed
--- src/Serializer/IntegerMoneySerializer.php
+++ src/Serializer/IntegerMoneySerializer.php
@@ -10,7 +10,7 @@ class IntegerMoneySerializer implements MoneySerializer
     /**
      * Formats a Money object as string.
      *
-     * @param  \Money\Money  $moeny
+     * @param  \Money\Money  $money
      * @return mixed
      */
     public function serialize(Money $money): mixed
