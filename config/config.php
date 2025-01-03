77e784f9014a19293bdfe4851e00b1ee838ea04d
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2025-01-03 10:03:39 -0300

    Fix iso currencies path

--- config/config.php
+++ config/config.php
@@ -10,7 +10,9 @@ return [
     'defaultCurrency' => config('app.currency', 'USD'),
     'defaultFormatter' => null,
     'defaultSerializer' => null,
-    'isoCurrenciesPath' => __DIR__.'/../vendor/moneyphp/money/resources/currency.php',
+    'isoCurrenciesPath' => is_dir(__DIR__.'/../vendor')
+        ? __DIR__.'/../vendor/moneyphp/money/resources/currency.php'
+        : __DIR__.'/../../../moneyphp/money/resources/currency.php',
     'currencies' => [
         'iso' => 'all',
         'bitcoin' => 'all',
